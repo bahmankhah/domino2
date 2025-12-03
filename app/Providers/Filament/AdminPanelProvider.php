32cafd61d1fff0ab0be5835105c3c418f37cdeb4
Hesam <bahmankhah1@gmail.com>
2025-12-04 02:26:24 +0330

    minor changes

--- app/Providers/Filament/AdminPanelProvider.php
+++ app/Providers/Filament/AdminPanelProvider.php
@@ -40,7 +40,6 @@ class AdminPanelProvider extends PanelProvider
             ->discoverWidgets(in: app_path('Filament/Widgets'), for: 'App\\Filament\\Widgets')
             ->widgets([
                 Widgets\AccountWidget::class,
-                Widgets\FilamentInfoWidget::class,
             ])
             ->middleware([
                 EncryptCookies::class,
@@ -55,6 +54,14 @@ class AdminPanelProvider extends PanelProvider
             ])
             ->authMiddleware([
                 Authenticate::class,
-            ]);
+            ])
+            ->authPasswordBroker('users')
+            ->homeUrl(function () {
+                $user = auth()->user();
+                if ($user && $user->role === 'driver') {
+                    return \App\Filament\Resources\OrderDeliveryResource::getUrl('index');
+                }
+                return null; // Default dashboard
+            });
     }
 }
